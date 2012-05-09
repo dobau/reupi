106f3f9b462e942a3ea837afaeb6f9d944569a72
dobau <rafaeldobau@gmail.com>
2012-05-08 21:24:09 -0300

    Comitando utiitarios utilizados no dia 8/5/2012

---
+++ oracle/adiciona_varias_colunas_em_tabela.sql
@@ -0,0 +1,9 @@
+-- Adicionando mais de uma coluna na tabela
+alter table
+   table_name
+add
+   (
+   column1_name column1_datatype column1_constraint,
+      column2_name column2_datatype column2_constraint,
+   column3_name column3_datatype column3_constraint
+      );
---
+++ oracle/remove_varias_colunas_em_tabela.sql
@@ -0,0 +1,5 @@
+-- Removendo varias colunas
+alter table
+   table_name
+drop
+   (col_name1, col_name2);
