298e9a2a9772f60b89f4030fce15eae52318ab70
Rafael Alves <rafaeldobau@gmail.com>
2017-10-19 21:00:46 -0300

    Create show_xml_call.java

---
+++ jaxws/show_xml_call.java
@@ -0,0 +1,4 @@
+		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
