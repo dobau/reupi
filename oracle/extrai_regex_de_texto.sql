acfdb7e9978b44824d041fcb0bd633837a6f9595
Rafael Alves <rafaeldobau@gmail.com>
2019-02-05 16:55:59 -0200

    Update extrai_regex_de_texto.sql

--- oracle/extrai_regex_de_texto.sql
+++ oracle/extrai_regex_de_texto.sql
@@ -1,3 +1,4 @@
+-- OPTION 1
 WITH PLACAS AS (
     select 'Asdahsudhasdh DTY-1212 aaa' texto from dual UNION ALL
     select 'Asd FVM-0713 dhasdh  aaa' texto from dual UNION ALL
@@ -5,3 +6,7 @@ WITH PLACAS AS (
 )
 
 select regexp_substr(texto, '\s\w\w\w-\d\d\d\d\s') from placas;
+
+
+-- OPTION 2
+select regexp_replace(a.TB0475_DS_DADOS, '(.*)(\d{4}\-\d{2}\-\d{2})(.*)', '\2') DATA_INICIAL from TABLE;
