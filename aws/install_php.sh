3c7126dfadf7ccfd710ae440af36a86f5aa4641f
Rafael Alves <rafaeldobau@gmail.com>
2019-05-26 17:44:28 -0300

    Create install_php.sh

---
+++ aws/install_php.sh
@@ -0,0 +1,8 @@
+#EC2
+
+yum -y install httpd php
+chkconfing httpd on
+service httpd start
+
+cd /var/www/html
+wget https://bitbucket.org/awsdevguru/awsdevassoc/raw/cd3108e6d85d08f561136505f372f2728048a562/07._EC2/UserData/index.php
