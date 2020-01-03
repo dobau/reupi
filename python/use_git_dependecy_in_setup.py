dee689634714e294d14eb142d763e00a4991496e
Rafael Alves <rafaeldobau@gmail.com>
2020-01-03 11:53:44 -0300

    Create use_git_dependecy_in_setup.py

---
+++ python/use_git_dependecy_in_setup.py
@@ -0,0 +1,2 @@
+# https://stackoverflow.com/questions/32688688/how-to-write-setup-py-to-include-a-git-repo-as-a-dependency
+dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0']
