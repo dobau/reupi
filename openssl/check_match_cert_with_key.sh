2e1213cb32485c3e9d217d2661db8caf242dfd22
Rafael Alves <rafaeldobau@gmail.com>
2025-05-28 08:20:42 -0300

    Create check_match_cert_with_key.sh

---
+++ openssl/check_match_cert_with_key.sh
@@ -0,0 +1,3 @@
+openssl pkey -in privateKey.key -pubout -outform pem | sha256sum
+openssl x509 -in certificate.crt -pubkey -noout -outform pem | sha256sum
+openssl req -in CSR.csr -pubkey -noout -outform pem | sha256sum
